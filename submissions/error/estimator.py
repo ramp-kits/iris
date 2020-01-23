47e5c7c5c78813cda6e10373ec4001385ba47a68
Lucy Liu <jliu176@gmail.com>
2020-01-23 16:34:53 +0100

    amend for sklearn workflow

---
+++ submissions/error/estimator.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+# test failing submission
+def get_estimator():
+    clf = RandomForestClassifier(n_estimators=1, max_leaf_nodes=2,
+                                 random_state=61
+    return clf
